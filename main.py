59d87a9dd8bb5e49b4ae25bf300b92c1bfb36482
PrinceCompany <102676884+PrinceCompany@users.noreply.github.com>
2022-07-07 19:28:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pyrogram import Client
 
-app = Client("my_account", plugins=dict(root="plugins"))
+a = 1086018
+b = '3c2f1a043c1a22d5b0af74b8268993d5'
+c = '5053843367:AAHR2tDQr4sygSqbSl5v_ihB6Hat_Pl2OTQ'
+
+app = Client(name="my_account",api_id=a,api_hash=b,bot_token=c,plugins=dict(root="plugins"))
 
 app.run()
